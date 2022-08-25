32ccc625882aaf842cd60172e88a1801881fc981
mk812y <mk812y@gmail.com>
2022-08-25 18:36:09 +0300

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Михаил Куприянов on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
